917673083033b39c98038ff312bdc8dc5195b813
Salman_JS <salman4javascript@gmail.com>
2023-03-18 19:25:34 +0530

    Upload documents in the given path name

--- src/Controller/Requests/Function.js
+++ src/Controller/Requests/Function.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const url = "http://localhost:3002"
 
+// Login User Handler!
 export async function loginUser(data, id){
     const credentials = {
         cs : id,
@@ -31,4 +32,26 @@ export async function getData(id, path){
             return err.response;
         }
     }
+}
+
+// Upload the file to the server in the respective path!
+export async function uploadFile(data, path, id){
+    const URL = "https://" + id + ".ngrok.io"; // Cannot use this with localhost:3000 as localhost have cors issue policy enabled!
+    const pathName = path.split("/")[1] + "/"; // Adding "/" as the path identifier for the server to upload the documents!
+
+    // Form Data with PathName
+    const uploadFile = new FormData();
+    uploadFile.append("uploadFile", data);
+    uploadFile.append("pathName", pathName);
+
+    try{
+        // Upload the file to the server!
+        const result = await axios.post("http://localhost:3200/upload", uploadFile);
+        return result;
+    } catch(err){
+        if(err.response && err.response.status){
+            return err.response;
+        }
+    }
+
 }
