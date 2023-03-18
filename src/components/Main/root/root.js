950a8e116ec58d57e30a7452311be9406a347a37
Salman_JS <salman4javascript@gmail.com>
2023-03-18 17:31:59 +0530

    Fixed bread crumb conflict with cabinet data

--- src/components/Main/root/root.js
+++ src/components/Main/root/root.js
@@ -1,6 +1,6 @@
 export const root = {
     sideData : ["Home", "Downloads", "Documents", "Trash"],
-    prop: ['New Folder', "Properties"],
+    prop: ['Upload File', 'New Folder', "Properties"],
     breadCrumb: 'Crumb',
     content: "content",
     noFiles: "No items to display!"
